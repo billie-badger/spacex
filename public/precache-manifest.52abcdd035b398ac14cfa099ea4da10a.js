cb6f80b1d4d8f6094b674aec17429a8e9061c5bf
Will Vatcher <will.vatcher@gmail.com>
2018-11-29 20:55:49 -0800

    Built for deployment to Heroku

---
+++ public/precache-manifest.52abcdd035b398ac14cfa099ea4da10a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "019be3224e7a4dc61bc2e9887a9c40d4",
+    "url": "/static/media/loadingrocket.019be322.gif"
+  },
+  {
+    "revision": "65d0543cbc5fb640e729d337661ac6b7",
+    "url": "/static/media/SpaceX-Logo.65d0543c.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6613a385e7c5ee1abf5a",
+    "url": "/static/js/main.6613a385.chunk.js"
+  },
+  {
+    "revision": "02362f28176a7a4ba98c",
+    "url": "/static/js/1.02362f28.chunk.js"
+  },
+  {
+    "revision": "6613a385e7c5ee1abf5a",
+    "url": "/static/css/main.3f31c200.chunk.css"
+  },
+  {
+    "revision": "a276d3ce7b061ac11320727d8e9d8ee5",
+    "url": "/index.html"
+  }
+];
